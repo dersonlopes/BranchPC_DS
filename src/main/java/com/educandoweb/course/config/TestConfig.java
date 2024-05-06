c1ef08921b99d339bbcc080da38d632bbb5d3de3
Anderson Ribeiro Lopes <anderson.r.lopes@bradesco.com.br>
2024-05-06 14:34:01 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -22,8 +22,11 @@ public class TestConfig implements CommandLineRunner {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Jonathan Red", "jkonathan@gmail.com", "9876654321", "123456");
+		User u4 = new User(null, "Alexa black", "alexa@gmail.com", "912345678", "123456");
+		User u5 = new User(null, "Nonata White", "nonata@gmail.com", "912436587", "123456");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5));
 		
 
 	}
