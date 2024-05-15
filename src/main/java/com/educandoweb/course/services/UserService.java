31dc24f6f282efd9dc15779883896beabf464494
Anderson Ribeiro Lopes <anderson.r.lopes@bradesco.com.br>
2024-05-15 13:37:19 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -9,32 +9,45 @@ import org.springframework.stereotype.Service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
-@Service 
+@Service
 public class UserService {
-	
+
 //buscar todos
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	
+
 //buscar por id
 	public User findById(Long id) {
-		Optional<User> obj =	repository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.get();
-		
+
 	}
+
 // incluir novo usuario
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 
 // Deletar um usuario por id
-	public void delete (Long id) {
+	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+// Atualizar um usuario 
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	// metodo de atualizacao chamado pelo update
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
